191858fe3760845b7ff0058b519e918c954a5a01
rohith <jayarohithvemula@gmail.com>
2022-03-03 12:06:08 +0530

    hashing password

--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ from typing import Optional,List
 from fastapi import FastAPI,Response,status,HTTPException,Depends
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from passlib.context import CryptContext
 import time
 from sqlalchemy.orm import Session
 from . import models, schemas
 from .database import engine, get_db
 
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -110,6 +112,10 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
 
 @app.post("/users",status_code=status.HTTP_201_CREATED,response_model=schemas.UserOut)
 def creat_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    #hash the password
+    hashed_password = pwd_context.hash(user.password)
+    user.password = hashed_password
     new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
