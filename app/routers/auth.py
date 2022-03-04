60f2d89df18ded81d14ebb0ed216b854184f67ee
rohith <jayarohithvemula@gmail.com>
2022-03-04 16:50:12 +0530

    creating tokens and OAuth2 PasswordRequestForm

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
-from .. import database, schemas,models,utils
+from .. import database, schemas,models,utils,oauth2
 
 router = APIRouter(tags=["Authentication"])
 
 @router.post("/login")
-def login(user_creadentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.email == user_creadentials.email).first()
+def login(user_creadentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.email == user_creadentials.username).first()
 
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
@@ -17,4 +18,7 @@ def login(user_creadentials: schemas.UserLogin, db: Session = Depends(database.g
 
     #create a token
     #return token
-    return {"token": "example token"}
+
+    access_token = oauth2.creat_access_token(data = {"user_id":user.id})
+
+    return {"access_token": access_token, "token_type": "bearer"}
